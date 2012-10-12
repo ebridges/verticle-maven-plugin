38e06d737a03ca95209e4a7bf6d815f8e24b48b6
Edward Bridges <ebridges@squarespace.com>
2012-10-12 16:25:57 -0400

    Expand to include all mod.json parameters, remove hardcoding. Fix copying of classes & resources

--- README.md
+++ README.md
@@ -1,20 +1,80 @@
-# verticle-archive
+# verticle-maven-plugin
 
 This is a Maven mojo that can be configured into the pom.xml of a vert.x project in order to automate the assembly of vert.x modules.
 
 ## Usage
 
-`mvn install`
+After downloading the most recent version (e.g. `${EXAMPLE_VERSION}` below) of the jar file, you can install it in your local environment as follows:
+<pre>
+mvn install:install-file \
+    -Dfile=verticle-maven-plugin-${EXAMPLE_VERSION}.jar \
+    -DgroupId=com.eqbridges.vertx \
+    -DartifactId=verticle-maven-plugin \
+    -Dversion=${EXAMPLE_VERSION} \
+    -Dpackaging=maven-plugin \
+    -DgeneratePom=true \
+    -DcreateChecksum=true \
+</pre>
 
-`mvn verticle:verticle`
+After it has been successfully installed locally, you'll need to configure it in your vert.x project's POM file.  Here's an example configuration that shows all configuration options with their defaults. All options as described in the module manual are supported.  (Visit that documentation)[http://vertx.io/mods_manual.html] for explanations of their usage.  None of the options are required.
+
+<pre>
+<project>
+...
+  <build>
+  ...
+    <pluginManagement>
+      <plugins>
+      ...
+        <plugin>
+          <groupId>com.eqbridges.vertx</groupId>
+          <artifactId>verticle-maven-plugin</artifactId>
+          <version>${EXAMPLE_VERSION}</version>
+          <executions>
+            <execution>
+              <goals>
+                <goal>verticle</goal>
+              </goals>
+            </execution>
+          </executions>
+          <configuration>
+              <verticleMain>com.example.VertxMain</verticleMain>
+              <worker>false</worker>
+              <preserveCwd>false</preserveCwd>
+              <autoRedeploy>false</autoRedeploy>
+              <includes>com.example.another-project-v0.0</includes>
+          </configuration>
+        </plugin>
+	...
+      </plugins>
+    </pluginManagement>
+    ...
+</project>
+</pre>
+
+Once your module's POM has been correctly configured, you assemble the verticle by typing:
+
+<pre>
+$ mvn verticle:assemble
+</pre>
+
+To capture and/or filter resources, enter:
+
+<pre>
+$ mvn process-resources verticle:assemble
+</pre>
 
 ## Notes
 
-* Dependency resolution is based on the dependency declarations in the pom.xml of the project. This mojo will copy into the verticle any dependency whose `scope` is equal to either `runtime` or `compile` (which is the default scope for a dependency).
+* Dependency resolution is based on the dependency declarations in the pom.xml of the project. This mojo will copy into the verticle any dependency whose `scope` is equal to either `runtime` or `compile` (which is the default scope for a dependency).  If a dependency is going to be available in the runtime environment (e.g. the vertx dependencies) it should be marked with a scope of `provided`.
+
+* The module copies the contents of the output directory (`${project.build.outputDirectory}` -- typically `target/classes`) into the root of the verticle.  This will capture any resources as well as any .class files and pull them into the verticle.  If resources need to be included and/or processed, then this should be run after the `mvn package` lifecycle phase.
+
+* If any scripts (i.e. non-Java) are part of the verticle, this script expects to find them under `src/main/scripts`.  Review the pom.xml documentation for ways to override, augment, or otherwise configure that.
 
 ## To Do
 
-* Expand to include all `mod.json` parameters, remove hardcoding.
+* -Expand to include all `mod.json` parameters, remove hardcoding.-
 * -Fix dependency resolution: currently pulls in all dependencies.-
 * -Clean up command line parameters-
 * Add option to zip up module folder for easier release/deploy.
--- src/main/java/com/eqbridges/vertx/VerticleModuleMojo.java
+++ src/main/java/com/eqbridges/vertx/VerticleModuleMojo.java
@@ -17,6 +17,8 @@ package com.eqbridges.vertx;
  */
 
 import static java.lang.String.format;
+import static java.nio.file.Files.isDirectory;
+import static java.util.Objects.requireNonNull;
 import static org.apache.commons.io.FileUtils.copyFileToDirectory;
 import static org.apache.commons.io.IOUtils.closeQuietly;
 import static org.apache.commons.lang.StringUtils.isEmpty;
@@ -26,22 +28,35 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.nio.file.FileVisitOption;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Collection;
-import java.util.List;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.model.Resource;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.project.MavenProject;
+import org.vertx.java.core.json.JsonObject;
 
 /**
  * Mojo which assembles a verticle module.
  *
- * @goal verticle
- * @execute phase="package"
+ * @goal assemble
+ * @phase=package
  * @requiresDependencyResolution compile+runtime
+ * @threadSafe
  */
 @SuppressWarnings("UnusedDeclaration")
 public class VerticleModuleMojo extends AbstractMojo {
@@ -50,6 +65,7 @@ public class VerticleModuleMojo extends AbstractMojo {
 
     /**
      * Location of the assembled module.
+     *
      * @parameter expression="${project.build.directory}"
      * @required
      */
@@ -58,23 +74,66 @@ public class VerticleModuleMojo extends AbstractMojo {
 
     /**
      * The name of the generated verticle.
-     * @parameter expression="${project.groupId}.${project.artifactId}-v${project.version}"
+     *
+     * @parameter default-value="${project.groupId}.${project.artifactId}-v${project.version}"
+     *            expression="${verticleName}"
      * @required
      */
     @SuppressWarnings("UnusedDeclaration")
     private String verticleName;
 
     /**
-     * The name of the main class for this verticle module.
-     * @parameter expression="${project.artifactId}"
-     * @required
+     * Runnable modules must contain a field main which specifies the main verticle to start the module. Main would be something like myscript.groovy, app.rb, foo.js or org.acme.MyApp.
+     * Non-runnable modules do not need a main field.
+     *
+     * @parameter default-value="" expression="${verticleMain}"
      */
     @SuppressWarnings("UnusedDeclaration")
     private String verticleMain;
 
     /**
+     * If your main verticle is a worker verticle you must also specify the worker field with value true, otherwise it will be assumed it is not a worker.
+     *
+     * @parameter default-value="false" expression="${worker}"
+     */
+    @SuppressWarnings("UnusedDeclaration")
+    private Boolean worker;
+
+    /**
+     * By default when your module is executing it will see its working directory as the module directory when using the Vert.x API. This is useful if you want to package up an application which contains its own static resources into a module.
+     * However in some cases, you don't want this behaviour, you want the module to see its working directory as the working directory of whoever started the module.
+     * If you want to preserve current working directory, set the field preserve-cwd to true in mod.json. The default value is false.
+     *
+     * @parameter default-value=false expression="${preserveCwd}"
+     */
+    @SuppressWarnings("UnusedDeclaration")
+    private Boolean preserveCwd;
+
+    /**
+     * You can configure a module to be auto-redeployed if it detects any files were modified, added or deleted in its module directory.
+     * To enable auto re-deploy for a module you should specify a field auto-redeploy with a value of true in mod.json. The default value for auto-redeploy is false.
+     *
+     * @parameter default-value=false expression="${autoRedeploy}"
+     */
+    @SuppressWarnings("UnusedDeclaration")
+    private Boolean autoRedeploy;
+
+    /**
+     * Sometimes you might find that different modules are using the same or similar sets of resources, e.g. .jar files or scripts, or other resources.
+     * Instead of including the same resources in every module that needs them, you can put those resources in a module of their own, and then declare that other modules includes them.
+     * The resources of the module will then effectively be put on the module path of the using module.
+     * This is done by specifying an includes field in the module descriptor.
+     *
+     * @parameter default-value=false expression="${includes}"
+     */
+    @SuppressWarnings("UnusedDeclaration")
+    private String includes;
+
+    /**
      * The Maven project.
-     * @parameter expression="${project}"
+     * @parameter default-value="${project}"
+     * @readonly
+     * @required
      */
     @SuppressWarnings("UnusedDeclaration")
     private MavenProject project;
@@ -90,31 +149,55 @@ public class VerticleModuleMojo extends AbstractMojo {
 
         createModJson(verticleFolder);
 
-        //noinspection unchecked
-        copyResources(project.getResources(), verticleFolder);
+        for(Object scriptSourceDir : project.getScriptSourceRoots()) {
+            if(null != scriptSourceDir && !scriptSourceDir.toString().isEmpty()) {
+                File scriptsDir = new File(scriptSourceDir.toString());
+                if(scriptsDir.exists()) {
+                    copyFiles(scriptsDir, verticleFolder);
+                }
+            }
+        }
+
+        String classesFolder = project.getModel().getBuild().getOutputDirectory();
+        File classesDir = new File(classesFolder);
+        if(classesDir.exists()) {
+            copyFiles(classesDir, verticleFolder);
+        }
 
         //noinspection unchecked
-        Set artifacts = project.getDependencyArtifacts();
-        //noinspection unchecked
+        Set<Artifact> artifacts = project.getDependencyArtifacts();
         copyDependencies(artifacts, verticleFolder);
     }
 
-    private void copyDependencies(Collection<Artifact> dependencies, File verticleFolder) throws MojoExecutionException {
-        File verticleLibFolder = new File(verticleFolder, MOD_LIB);
-        if(!verticleLibFolder.exists()) {
-            if(!verticleLibFolder.mkdirs()) {
-                throw new MojoExecutionException("unable to create verticleLibFolder: " + verticleLibFolder);
-            }
+    private void copyFiles(File fromDir, File verticleFolder) throws MojoExecutionException {
+        Path from = Paths.get(fromDir.toURI());
+        Path to = Paths.get(verticleFolder.toURI());
+        try {
+            Files.walkFileTree(from, EnumSet.of(FileVisitOption.FOLLOW_LINKS),Integer.MAX_VALUE,new CopyDirVisitor(from, to, getLog()));
+        } catch (IOException e) {
+            throw new MojoExecutionException("unable to copy classes to verticleFolder", e);
         }
-        for(Artifact dependency : dependencies) {
-            if(null != dependency && null != dependency.getFile()) {
-                if(confirmDependencyScope(dependency.getScope())) {
-                    File dependencyFile = dependency.getFile();
-                    try {
-                        getLog().info(format("Copying dependency: [%s][%s] -> [%s]", dependencyFile, dependency.getScope(), verticleLibFolder));
-                        copyFileToDirectory(dependencyFile, verticleLibFolder);
-                    } catch (IOException e) {
-                        throw new MojoExecutionException("unable to copy dependency ("+dependencyFile+") to ("+verticleLibFolder+"): " + e.getMessage());
+    }
+
+    private void copyDependencies(Collection<Artifact> dependencies, File verticleFolder) throws MojoExecutionException {
+        if(null != dependencies && !dependencies.isEmpty()) {
+            File verticleLibFolder = new File(verticleFolder, MOD_LIB);
+            if(!verticleLibFolder.exists()) {
+                if(!verticleLibFolder.mkdirs()) {
+                    throw new MojoExecutionException("unable to create verticleLibFolder: " + verticleLibFolder);
+                }
+            }
+
+            for(Artifact dependency : dependencies) {
+                if(null != dependency && null != dependency.getFile()) {
+                    if(confirmDependencyScope(dependency.getScope())) {
+                        File dependencyFile = dependency.getFile();
+                        try {
+                            getLog().info(format("Copying dependency: [%s][%s] -> [%s]", dependencyFile.getName(), dependency.getScope(), "/"+verticleName+"/"+MOD_LIB));
+                            copyFileToDirectory(dependencyFile, verticleLibFolder);
+                        } catch (IOException e) {
+                            throw new MojoExecutionException("unable to copy dependency ("+dependencyFile+") to ("+verticleLibFolder+"): " + e.getMessage());
+                        }
                     }
                 }
             }
@@ -130,12 +213,12 @@ public class VerticleModuleMojo extends AbstractMojo {
         return false;
     }
 
-    private void copyResources(List<Resource> resources, File verticleFolder) throws MojoExecutionException {
+    private void copyResources(Collection<Resource> resources, File verticleFolder) throws MojoExecutionException {
         for(Resource resource : resources) {
             if(null != resource && !isEmpty(resource.getTargetPath())) {
                 File resourceFile = new File(resource.getTargetPath());
                 try {
-                    getLog().info(format("Copying resource: [%s] -> [%s]", resourceFile, "/"+verticleName+"/"+MOD_LIB));
+                    getLog().info(format("Copying resource: [%s] -> [%s]", resourceFile.getName(), "/"+verticleName));
                     copyFileToDirectory(resourceFile, verticleFolder);
                 } catch (IOException e) {
                     throw new MojoExecutionException("unable to copy resource ("+resourceFile+") to ("+verticleFolder+"): " + e.getMessage());
@@ -145,23 +228,89 @@ public class VerticleModuleMojo extends AbstractMojo {
     }
 
     private void createModJson(File verticleFolder) throws MojoExecutionException {
+        String json = modJson();
         File modJson = new File(verticleFolder,MOD_JSON);
         Writer w=null;
         try {
             w = new BufferedWriter(new FileWriter(modJson));
-            w.write(modJson());
+            w.write(json);
             w.flush();
+            getLog().info(format("Configuration written to: [%s]", modJson.getAbsolutePath()));
         } catch (IOException e) {
             throw new MojoExecutionException("unable to create module descriptor ("+modJson+"): " + e.getMessage());
         } finally {
-            if(null != w) {
-                closeQuietly(w);
-            }
+            closeQuietly(w);
         }
     }
 
     private String modJson() {
-        String json = "{\n\t\"main\": \"%s\"\n}\n";
-        return format(json, verticleMain);
+        Map<String, Object> params = new HashMap<String, Object>();
+
+        if(null != verticleMain && !verticleMain.isEmpty()) {
+            getLog().info(format("Config param [%s]:[%s]", "main", verticleMain));
+            params.put("main", verticleMain);
+        }
+
+        if(null != worker && worker) {
+            getLog().info(format("Config param [%s]:[%s]", "worker", worker));
+            params.put("worker", worker);
+        }
+
+        if(null != preserveCwd && preserveCwd) {
+            getLog().info(format("Config param [%s]:[%s]", "preserve-cwd", preserveCwd));
+            params.put("preserve-cwd", preserveCwd);
+        }
+
+        if(null != autoRedeploy && autoRedeploy) {
+            getLog().info(format("Config param [%s]:[%s]", "auto-redeploy", autoRedeploy));
+            params.put("auto-redeploy", autoRedeploy);
+        }
+
+        if(null != includes && !includes.isEmpty()) {
+            getLog().info(format("Config param [%s]:[%s]", "includes", includes));
+            params.put("includes", includes);
+        }
+
+        return new JsonObject(params).toString();
+    }
+}
+
+class CopyDirVisitor extends SimpleFileVisitor<Path> {
+    private Path fromPath;
+    private Path toPath;
+    private static final StandardCopyOption copyOption = StandardCopyOption.REPLACE_EXISTING;
+    private final Log log;
+
+    CopyDirVisitor(Path fromPath, Path toPath, Log log) {
+        validate(fromPath);
+        this.fromPath = fromPath;
+        this.toPath = toPath;
+        this.log = log;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        Path targetPath = toPath.resolve(fromPath.relativize(dir));
+        if(!Files.exists(targetPath)){
+            Files.createDirectory(targetPath);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        Path to = toPath.resolve(fromPath.relativize(file));
+        Files.copy(file, to, copyOption);
+        log.info(format("Copied file [%s] -> [%s]", file.getFileName(), to.normalize()));
+        return FileVisitResult.CONTINUE;
+    }
+
+    private static void validate(Path... paths) {
+        for (Path path : paths) {
+            requireNonNull(path);
+            if (!isDirectory(path)) {
+                throw new IllegalArgumentException(String.format("%s is not a directory", path.toString()));
+            }
+        }
     }
 }
